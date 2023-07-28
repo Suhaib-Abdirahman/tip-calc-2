857a86a3839798762263576384d4fc332808d089
Suhaib Abdirahman <139591958+Suhaib-Abdirahman@users.noreply.github.com>
2023-07-28 11:01:25 +0300

    tip-calc-2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello");
+function calcTip(bill) {
+    let tip;
+
+    if (bill >= 50 && bill <= 300)
+      tip = bill * 0.15;
+    else if (bill > 300)
+      tip = bill * 0.20;
+
+    const total = bill + tip;
+    return `The bill was ${bill}, the tip was ${tip}, and the total value ${total}`;
+  }
+
+  const bill = 289;
+  const result = calcTip(bill);
+  console.log(result);
